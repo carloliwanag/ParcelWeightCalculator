8f0ce236dea1433e2e62208741c2870d98224d9f
carlo <ecaliwanag@gmail.com>
2021-11-14 17:39:19 +0800

    changed to Double

--- src/main/java/com/carloliwanag/exam1/service/ParcelCalculatorService.java
+++ src/main/java/com/carloliwanag/exam1/service/ParcelCalculatorService.java
@@ -1,6 +1,11 @@
 package com.carloliwanag.exam1.service;
 
+import java.util.List;
+
+import com.carloliwanag.exam1.utils.RuleName;
+
 public interface ParcelCalculatorService {
-	Integer computeVolume(Integer height, Integer length, Integer width);
-	Integer computeCost(Integer height, Integer length, Integer width);
+	Double computeVolume(Double height, Double length, Double width);
+	Double computeCost(Double height, Double length, Double width, Double weight);
+	List<RuleName> determineRule(Double volume, Double weight); 
 }
