97a4d38f4ea5c278190037c4d6a5cf35bbb99992
carlo <ecaliwanag@gmail.com>
2021-11-15 16:38:13 +0800

    make promo code optional

--- src/main/java/com/carloliwanag/exam1/controller/ParcelCalculatorController.java
+++ src/main/java/com/carloliwanag/exam1/controller/ParcelCalculatorController.java
@@ -1,8 +1,9 @@
 package com.carloliwanag.exam1.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class ParcelCalculatorController {
 	}
 	
 	@GetMapping("/compute")
-	public Double computeCost(@RequestParam Double length, @RequestParam Double width, @RequestParam Double height, @RequestParam Double weight, @RequestParam String promoCode) {
-		return this.parcelCalculatorService.computeCost(height, length, width, weight, promoCode);
+	public Double computeCost(@RequestParam Double length, @RequestParam Double width, @RequestParam Double height, @RequestParam Double weight, @RequestParam Optional<String> promoCode) {
+		return this.parcelCalculatorService.computeCost(height, length, width, weight, promoCode.isPresent() ? promoCode.get() : null);
 	}
 }
